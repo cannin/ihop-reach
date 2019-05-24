c7c06dc93c24a07a0e9367f020de1b5e0b2a29bc
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-05-24 20:12:32 +0530

    Added Identifier Endpoint and Swagger Documentation
    Added endpoint GET articles/identifier/:key for searching articles using identifier in extracted data.
    
    Added Swagger for API documentation

--- api/api.py
+++ api/api.py
@@ -1,35 +1,62 @@
 import os
 from eve import Eve
+from flask import redirect
 from gevent.pywsgi import WSGIServer
+
+from eve_swagger import swagger # Swagger for API
 
 # Database event hooks
 
 #function to delete _created and _updated from resource dictionary
-def on_fetched_resource(resource, response):    
+def on_fetched_resource(response):
     response['items'] = response.pop('_items')
     for doc in response['items']:
         for field in doc.keys():
             if field.startswith('_'):
-                if(field != '_id'):
-                    del(doc[field])
+                if field != '_id':
+                    del doc[field]
                 else:
                     doc['id'] = doc.pop(field) #Change name of id key in response
 
-#function to delete _created and _updated from item dictionary                    
-def on_fetched_item(response):   
-    del(response['_updated'])               # Delete _updated key in response
-    del(response['_created'])               # Delete _created key in response
-    response['id'] = response.pop('_id')    # Change name of id key in response
-    
+#function to delete _created and _updated from item dictionary
+def on_fetched_item(response):
+    del response['_updated']              # Delete _updated key in response
+    del response['_created']              # Delete _created key in response
+    del response['_links']               # Delete _links Hateos key in response
+    response['id'] = response.pop('_id')   # Change name of id key in response
+
+# Create a pyeve instance
 app = Eve("iHOP API")
-#attaching the hooks to events
+
+# Uncomment the next line to expose /api-docs where JSON of the
+# API for creating documention will be available
+# app.register_blueprint(swagger)
+
+# attaching event hook with functions
 app.on_fetched_resource += on_fetched_resource
 app.on_fetched_item_articles += on_fetched_item
 
-if __name__ == '__main__':    
-    port = int(os.environ.get('PORT', 5000))    # Bind to PORT if defined, otherwise default to 5000.
+# Swagger API documentation options
+app.config['SWAGGER_INFO'] = {
+    'title': 'iHOP Reach API',
+    'version': '1.0',
+    'description': 'An API to access the NLP Extracted Interaction Network',
+    'schemes': ['http', 'https'],
+}
+
+# endpoint for identifier search
+@app.route('/api/v1/articles/identifier/<key>')
+def identifier_redirect(key):
+    # The endpoint is permanent(301) redirect to article endpoint with
+    # where clause using a OR for filtering either fields
+    return redirect('/api/v1/articles?where={%22$or%22:[{%22extracted_information.participant_a.identifier%22:%22'+key+'%22},{%22extracted_information.participant_b.identifier%22:%22' + key + '%22}]}', code=301)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000
+    PORT = int(os.environ.get('PORT', 5000))
+
     # Debug/Development
-    # app.run(debug=True, host="0.0.0.0", port=port)
+    # app.run(port=PORT)
     # Production
-    http_server = WSGIServer(('', port), app)
-    http_server.serve_forever()
+    HTTP_SERVER = WSGIServer(('', PORT), app)
+    HTTP_SERVER.serve_forever()
