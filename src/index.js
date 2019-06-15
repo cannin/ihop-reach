7f8acc7c98d37c010c8ec12a45f5c57f4f2a561f
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-06-15 20:49:21 +0530

    Added filtering, Welcome Msg, Documentation
    Added Filtering in allDocuments query
    Added Welcome message
    Improved documentation by adding examples

--- src/index.js
+++ src/index.js
@@ -18,8 +18,95 @@ const context = () => MongoClient.connect('mongodb://localhost:27017', {
 const schema = buildSchema(`
     "List of GraphQL Queries the API supports"
     type Query {
+        """
+            It returns all documents( maximum 250 per page) present in the database
+
+            Example:
+            
+            {
+            
+                allDocuments(iden: "uniprot:Q16595") {
+            
+                    pmc_id
+            
+                    extracted_information {
+            
+                        participant_b {
+            
+                            identifier
+            
+                        }
+            
+                        participant_a {
+            
+                            identifier
+            
+                        }
+            
+                    }
+            
+                }
+            
+            }
+
+            The query will return pmc_id and participant information of documents having identifier uniprot:Q16595
+        """
+        allDocuments(            
+            "Page Number"
+            page: Int = 1
+            "Hypothesis Information"
+            hypothesis: Boolean 
+            "Interaction Type"     
+            interactionType : String
+            "Negative Information"
+            negInfo : Boolean
+            "Participant Entity Text"
+            entityText: String
+            "Participant Entity Type"
+            entityType : String
+            "Exact match PMCID of Article"
+            pmc : String
+            "Triggering phrase"
+            trigger : String
+            "Sentence in the article"
+            evidence : String
+            "Context Species"
+            species : String
+            "Exact match Identifier of participant entity"
+            iden : String
+        ): [Document]
+
+		"""
+            It returns single document matching the  Object ID
+
+            Example:
+
+            {
+            
+                document(id: "5d00179ce3318daa924be884") {
+                
+                    pmc_id
+                
+                }
+            
+            }  
+
+            The query returns pmc_id of the document specifed as argument
+        """
+        document(
+            """
+            Object ID(_id) of the document
+
+            eg *5d00179ce3318daa924be883*
+            """
+            id: ID!
+        ): Document
+
 		"Returns all the articles of the specified identifier"
-        articlesByIdentifier(id: String!): Entities
+        articlesByIdentifier(
+            "Identifier of the Entity"
+            id: String!
+        ): Entities
 
 		"It returns all unique identifiers present in database"
         uniqueIdentifiers: [String]
@@ -28,20 +115,17 @@ const schema = buildSchema(`
         allIdentifiers : [IdentifierDetails]
 
 		"It returns Entity details by identifier"
-        identifier(id: String!) : IdentifierDetails
-
-		"It returns all articles(250 per page) present in the database"
-        allArticles(page: Int): [Article]
-
-		"It returns single article matching the document Object ID"
-        article(id: ID!): Article
+        identifier(
+            "Identifier of the Entity"
+            id: String!
+        ): IdentifierDetails
     }
 	
-	"Object type for articlesByIdentifier query. Returns all articles consisting the searched identifier."    
+	"Object type for documentByIdentifier query. Returns all articles consisting the searched identifier."    
     type Entities {
         count : Int
         searchkey : String
-        articles: [Article]
+        articles: [Document]
     }
 	"Object type for identifier and allIdentifiers queries. Returns identifier details."
     type IdentifierDetails {
@@ -50,7 +134,7 @@ const schema = buildSchema(`
         typ: String
     }
 	"Object type for article and allArticles queries. Returns all Biomedical details from the extracted article details."
-    type Article {	
+    type Document {	
         _id: ID
         "Relevant text from the article."        
         evidence: [String]
@@ -143,7 +227,96 @@ async function uniIden(db) {
 const collection = "articles" // Set collection name
 const dbName = 'iHOP'
 const resolvers = {
-    //	Returns all the articles of the specified identifier
+
+    //	It returns all Document(250 per page) present in the database
+    allDocuments: (args, context) => context().then(async client => {
+        let db = client.db(dbName)
+        const fieldArr = {
+            hypothesis : "extracted_information.hypothesis_information",
+            interactionType : "extracted_information.interaction_type",
+            negInfo : "extracted_information.negative_information",
+            pmc : "pmc_id",
+            trigger : "trigger",
+            evidence : "evidence",
+            species : "extracted_information.context.Species",
+            // entityText : "extracted_information.participant_a/b.entity_text",
+            // entityType : "extracted_information.participant_a/b.entity_type",
+            // iden : "extracted_information.participant_a/b.identifier"
+        }
+        let andArray = []
+        let orArray = []
+        for ( let arg in args ){
+            switch (arg) {
+                case "page":                    
+                    break
+                case "entityType":
+                    orArray = []
+                    orArray.push(
+                        {
+                            "extracted_information.participant_a.entity_type" : args[arg]
+                        },
+                        {
+                            "extracted_information.participant_b.entity_type" : args[arg]
+                        }
+                    )
+                    andArray.push({
+                        "$or" : orArray
+                    })
+                    break
+                case "entityText":
+                    orArray = []
+                    orArray.push(
+                        {
+                            "extracted_information.participant_a.entity_text" : args[arg]
+                        },
+                        {
+                            "extracted_information.participant_b.entity_text" : args[arg]
+                        }
+                    )
+                    andArray.push({
+                        "$or" : orArray
+                    })
+                    break
+                case "iden":
+                    orArray = []
+                    orArray.push(
+                        {
+                            "extracted_information.participant_a.identifier" : args[arg]
+                        },
+                        {
+                            "extracted_information.participant_b.identifier" : args[arg]
+                        }
+                    )
+                    andArray.push({
+                        "$or" : orArray
+                    })
+                    break
+                default:
+                    let fieldObj = {}
+                    fieldObj[fieldArr[arg]] = args[arg]
+                    andArray.push(fieldObj)
+                    break
+            }
+        }
+        let filter = {}
+        if(andArray.length>0)
+            filter["$and"] = andArray
+        let res = await db.collection(collection).find(filter).skip(args.page < 1 ? 0 : (args.page - 1) * 250 || 1).limit(250).toArray()
+        client.close()
+        return res
+        }
+    ),
+
+    //	It returns single document matching the document Object ID
+    document: (args, context) => context().then(async client => {
+        let db = client.db(dbName)
+        let res = await db.collection(collection).findOne(ObjectId(args.id))
+        client.close()
+        return res
+    }),
+
+    // Returns all the document of the specified identifier
+    // TODO change to documentByIdentifier after making necessory changes in GatsbyJS
     articlesByIdentifier: (args, context) => context().then(client => {
         let db = client.db(dbName)
         const id = args.id.trim()
@@ -180,35 +353,110 @@ const resolvers = {
         })
     },
     //	It returns details of all Entities present in database by identifiers.
-    allIdentifiers: (args, context) => context().then(client => {
-        let db = client.db(dbName)
-        let res = db.collection("identifier_mapping").find().toArray()
+    allIdentifiers: (args, context) => context().then(async client => {
+        let db = client.db(dbName)        
+        let res = await db.collection("identifier_mapping").find().toArray()
         client.close()
         return res
     }),
     //	It returns Entity details by identifier
-    identifier: (args, context) => context().then(db => db.collection("identifier_mapping").findOne({
-        "iden": args.id
-    })),
-    //	It returns all articles(250 per page) present in the database
-    allArticles: (args, context) => context().then(db => db.collection(collection).find().skip(args.page < 1 ? 0 : (args.page - 1) * 250 || 1).limit(250).toArray()),
-    //	It returns single article matching the document Object ID
-    article: (args, context) => context().then(client => {
+    identifier: (args, context) => context().then(async client => {
         let db = client.db(dbName)
-        let res = db.collection(collection).findOne(ObjectId(args.id))
+        let res = await db.collection("identifier_mapping").findOne({"iden": args.id})
         client.close()
         return res
-    })
-
+        }
+    )
 };
+const welcomeMsg = `
+# Welcome to REACH GraphQL API explorer
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that starts
+# with a # are ignored.
+#
+# Keyboard shortcuts:
+#
+#  Prettify Query:  Shift-Ctrl-P (or press the prettify button above)
+#       Run Query:  Ctrl-Enter (or press the play button above)
+#   Auto Complete:  Ctrl-Space (or just start typing)
+#
+# At the top right corner of your screen you can find "Docs", clicking on
+# which will open Documentation Explorer which lets you access the definations and accepted arguments by the
+# query functions.
+#
+# Following is an example query which returns pmc_id and participant_a 
+# information of the documents in the database
 
+{
+  allDocuments{
+    pmc_id
+    extracted_information{
+      participant_a{
+        entity_text
+        identifier
+      }
+    }
+  }
+}
+
+# Following is an example of a REACH file as stored in our database
+# {
+#     "_id" : ObjectId("5d00162be3318daa924b9959"),
+#     "submitter" : "Reach",
+#     "model_relation" : "extension",
+#     "extracted_information" : {
+#         "interaction_type" : "decreases_activity",
+#         "negative_information" : false,
+#         "hypothesis_information" : false,
+#         "context" : {
+#             "Species" : [ 
+#                 "taxonomy:10535"
+#             ]
+#         },
+#         "participant_b" : [ 
+#             {
+#                 "in_model" : true,
+#                 "identifier" : "uniprot:P29972",
+#                 "entity_text" : "AQP1",
+#                 "entity_type" : "protein"
+#             }
+#         ],
+#         "participant_a" : [ 
+#             {
+#                 "in_model" : true,
+#                 "identifier" : "interpro:IPR000818",
+#                 "entity_text" : "TEA",
+#                 "entity_type" : "family"
+#             }
+#         ]
+#     },
+#     "reading_complete" : "2016-01-08T14:49:54Z",
+#     "reader_type" : "machine",
+#     "reading_started" : "2016-01-08T14:48:55Z",
+#     "trigger" : "Block",
+#     "evidence" : [ 
+#         "Block by TEA of AQP1"
+#     ],
+#     "pmc_id" : "100781",
+#     "score" : 0
+# }
+`
 // Starting the application
 const app = express();
 const PORT = process.env.PORT || 8080
 app.use('/', graphqlHTTP({
     schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: {
+        defaultQuery: welcomeMsg
+    },
     context
 }));
 app.listen(PORT);
