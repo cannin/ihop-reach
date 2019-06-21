80768dad6f81d46f587925b53965797e66f9d5a7
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-06-21 19:47:48 +0530

    Added Caching for Search_Index

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -60,6 +60,38 @@ module.exports = {
         },
         filename: 'search_index.json'
       }
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        cacheId: `reach-sw`,
+        // Don't cache-bust JS or CSS files, and anything in the static directory,
+        // since these files have unique URLs and their contents will never change
+        dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`,
+          },
+          {
+            // Search in cache and then revalidate in network
+            urlPattern: /search_index\.json/,
+            handler: `staleWhileRevalidate`,
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`,
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: `staleWhileRevalidate`,
+          },
+        ]
+      }
     }
   ],
 }
