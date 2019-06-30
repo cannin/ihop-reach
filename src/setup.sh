681cf5934240af33a19008443cb6e4bee2aac857
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-07-01 02:47:19 +0530

    Remove unnecessary indexing from setup script

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,7 @@
 mkdir -p /data/db
 touch /var/log/mongodb.log
 chmod 777 /var/log/mongodb.log
-echo "STEP 1/6 Starting MongoDB"
+echo "STEP 1/5 Starting MongoDB"
 # Start mongodb with logging
 # --logpath    Without this mongod will output all log information to the standard output.
 # --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
@@ -21,31 +21,19 @@ while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
 done
 
 # Unzip source
-echo "STEP 2/6 Unzip the downloaded Dataset"
+echo "STEP 2/5 Unzip the downloaded Dataset"
 unzip /src/dataset.zip -d /src/unzipDestDataset
 
 # Restore from dump
-echo "STEP 3/6 Mongo restore initiated"
+echo "STEP 3/5 Mongo restore initiated"
 mongorestore --drop /src/unzipDestDataset
 
-# Create and Rebuild indexes
-echo "STEP 4/6 Creating Indexes"
-mongo <<EOF
-use iHOP
-db.getCollection("articles").createIndex({"extracted_information.participant_b.identifier" : 1},{name: "idenB"})
-db.getCollection("articles").createIndex({"extracted_information.participant_a.identifier" : 1},{name: "idenA"})
-db.getCollection("articles").createIndex({"extracted_information.participant_b.entity_text" : 1},{name: "entityNameB"})
-db.getCollection("articles").createIndex({"extracted_information.participant_a.entity_text" : 1},{name: "entityNameA"})
-db.getCollection("identifier_mapping").createIndex({"iden" : 1},{name: "identifier"})
-db.getCollection("pubmed").createIndex({"pmcid" : -1},{name: "pmcid_index"})
-EOF
-
 # Delete  source files bash file
-echo "STEP 5/6 Removing temporary files"
+echo "STEP 4/5 Removing temporary files"
 rm -rf /src/unzipDestDataset
 rm /src/setup.sh
 
-echo "STEP 6/6 Starting Web application"
+echo "STEP 5/5 Starting Web application"
 # Install Node Modules
 npm install
 # Starting application
