4fd891916f996570c39fabaa12d6bafbe48d210f
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-07-07 04:00:23 +0530

    Add GraphiQL-Explorer
    Related Issue: #51

---
+++ src/server/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import { resolvers, context } from './resolvers'
+import compression from 'compression'
+import graphqlHTTP from 'express-graphql'
+import graphQL_Schema from './schema.graphql'
+import { buildSchema } from 'graphql'
+import cors from 'cors'
+import path from 'path'
+
+const schema = buildSchema(graphQL_Schema)
+const app = express()
+app.use(compression())
+app.use(cors())
+
+const PORT = process.env.PORT || 8080
+
+const react_build_dir = path.join(__dirname, '../graphiql/build')
+app.use('/favicon.ico',(req,res)=>res.sendFile(react_build_dir + req.originalUrl))
+app.use(
+  '*',
+  (req,res)=>{
+    
+    // respond with html page
+    if (req.accepts('html')) {
+      res.status(200).sendFile(react_build_dir + req.originalUrl, (err) =>{
+          if(err){
+            res.sendFile(react_build_dir + '/index.html');
+          }
+        } 
+      )
+    }
+    // respond with json
+    else{
+      const graphqlFunc = graphqlHTTP({
+          schema,
+          rootValue: resolvers,
+          graphiql: false,
+          context
+        }  
+      )
+      return graphqlFunc(req,res)
+    }
+  }
+)
+
+app.listen(PORT)
+
+console.log(`Server ready at http://localhost:${PORT}/`)
--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,7 @@
 mkdir -p /data/db
 touch /var/log/mongodb.log
 chmod 777 /var/log/mongodb.log
-echo "STEP 1/5 Starting MongoDB"
+echo "STEP 1/6 Starting MongoDB"
 # Start mongodb with logging
 # --logpath    Without this mongod will output all log information to the standard output.
 # --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
@@ -21,19 +21,27 @@ while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
 done
 
 # Unzip source
-echo "STEP 2/5 Unzip the downloaded Dataset"
+echo "STEP 2/6 Unzip the downloaded Dataset"
 unzip /src/dataset.zip -d /src/unzipDestDataset
 
 # Restore from dump
-echo "STEP 3/5 Mongo restore initiated"
+echo "STEP 3/6 Mongo restore initiated"
 mongorestore --drop /src/unzipDestDataset
 
 # Delete  source files bash file
-echo "STEP 4/5 Removing temporary files"
+echo "STEP 4/6 Removing temporary files"
 rm -rf /src/unzipDestDataset
 rm /src/setup.sh
 
-echo "STEP 5/5 Starting Web application"
+echo "STEP 5/6 Building React Web application"
+cd graphiql
+# Install Node Modules
+npm install
+# Starting application
+npm run build
+
+echo "STEP 6/6 Starting API Server"
+cd ../server
 # Install Node Modules
 npm install
 # Starting application
