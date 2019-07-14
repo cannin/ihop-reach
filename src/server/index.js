8c48ea20f8349784e1eeab6198cbc2776a8e1f8a
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-07-15 04:49:48 +0530

    Improve Analytics

--- src/server/index.js
+++ src/server/index.js
@@ -1,54 +1,72 @@
-import express from 'express'
-import compression from 'compression'
-import graphqlHTTP from 'express-graphql'
-import { buildSchema } from 'graphql'
-import cors from 'cors'
-import path from 'path'
-import useragent from 'express-useragent'
-import uuidv3 from 'uuid/v3'
-import { resolvers, context } from './resolvers'
-import graphQL_Schema from './schema.graphql'
+import express from "express";
+import compression from "compression";
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
+import cors from "cors";
+import path from "path";
+import useragent from "express-useragent";
+import uuidv3 from "uuid/v3";
+import { resolvers, context } from "./resolvers";
+import graphQL_Schema from "./schema.graphql";
 
-const schema = buildSchema(graphQL_Schema)
-const app = express()
-app.use(compression())
-app.use(cors())
+const schema = buildSchema(graphQL_Schema);
+const app = express();
+app.use(compression());
+app.use(cors());
 app.use(useragent.express());
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const react_build_dir = path.join(__dirname, '../graphiql/build')
-app.use('/favicon.ico',(req,res)=>res.sendFile(react_build_dir + req.originalUrl))
-app.use(
-  '*',
-  (req,res)=>{
-    let uuid = uuidv3(JSON.stringify(req.useragent),'042ffd34-d989-321c-ad06-f60826172424')    
-    // respond with html page
-    if (req.useragent.browser != 'node-fetch' && req.accepts('html') && req.method == 'GET') {
-      res.status(200).sendFile(react_build_dir + req.originalUrl, (err) =>{
-          if(err){
-            res.sendFile(react_build_dir + '/index.html');
-          }
-        } 
-      )
-    }
-    // respond with json
-    else{
-      const graphqlFunc = graphqlHTTP({
-          schema,
-          rootValue: resolvers,
-          graphiql: false,
-          context : {
-            db : context,
-            uuid : uuid
-          }
-        }  
-      )
-      return graphqlFunc(req,res)
-    }
+const react_build_dir = path.join(__dirname, "../graphiql/build");
+app.use("/favicon.ico", (req, res) =>
+  res.sendFile(react_build_dir + req.originalUrl)
+);
+app.use("*", (req, res) => {
+  let uuid = null;
+  let client_ip = null;
+  // Check if GatsbyJS Build
+  let isGatsbyBuild = false;
+  if (req.useragent.browser == "node-fetch") isGatsbyBuild = true;
+  // In case it is a GatsbyJS Build
+  else {
+    client_ip = (
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      "&aip=1"
+    )
+      .split(",")[0]
+      .trim();
+    // aip anonymizes the IP address, this will prevent logging of empty IP address
+    uuid = uuidv3(
+      JSON.stringify(req.useragent) + client_ip,
+      "042ffd34-d989-321c-ad06-f60826172424"
+    ); //second parameter is random
   }
-)
+  // respond with html page
+  if (isGatsbyBuild == false && req.accepts("html") && req.method == "GET") {
+    res.status(200).sendFile(react_build_dir + req.originalUrl, err => {
+      if (err) {
+        res.sendFile(react_build_dir + "/index.html");
+      }
+    });
+  }
+  // respond with json
+  else {
+    const graphqlFunc = graphqlHTTP({
+      schema,
+      rootValue: resolvers,
+      graphiql: false,
+      context: {
+        db: context,
+        isGatsbyBuild: isGatsbyBuild,
+        uuid: uuid,
+        ip: client_ip
+      }
+    });
+    return graphqlFunc(req, res);
+  }
+});
 
-app.listen(PORT)
+app.listen(PORT);
 
-console.log(`Server ready at http://localhost:${PORT}/`)
+console.log(`Server ready at http://localhost:${PORT}/`);
--- src/server/resolvers.js
+++ src/server/resolvers.js
@@ -1,118 +1,126 @@
-import { MongoClient } from 'mongodb'
-import request from 'request'
+import { MongoClient } from "mongodb";
+import request from "request";
 
 //Connection to MongoDB Database
 const context = () =>
-  MongoClient.connect('mongodb://localhost:27017', {
-    useNewUrlParser: true,
-  }).then(client => client)
+  MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true
+  }).then(client => client);
 
-const collection = 'articles' // Set collection name
-const dbName = 'iHOP'
+const collection = "articles"; // Set collection name
+const dbName = "iHOP";
 
-const analytics = (uuid,query) => {
-	const GA_ID = 'UA-57486113-9'
-  	request.post({url:'https://www.google-analytics.com/collect', body: `v=1&t=pageview&tid=${GA_ID}&cid=${uuid}&dp=api/${query}`}, function callback(err) {
-    if (err) {
-      return console.error('Analytics failed:', err);
+const analytics = (context, query) => {
+  //Skip analytics in case of GatsbyJS Build process
+  if (context.isGatsbyBuild == true) return;
+  const GA_ID = "UA-57486113-9";
+  const client_ip = context.ip;
+  const uuid = context.uuid;
+  const payload = `v=1&t=pageview&tid=${GA_ID}&ni=1&dt=${query}&ds=API&cid=${uuid}&dp=api/${query}&uip=${client_ip}`;
+  request.post(
+    { url: "https://www.google-analytics.com/collect", body: payload },
+    err => {
+      if (err) {
+        return console.error("Analytics failed:", err);
+      }
     }
-  });
-}
+  );
+};
 
 const resolvers = {
   //	It returns all Document(250 per page) present in the database
   allDocuments: (args, context) =>
     context.db().then(async client => {
-      analytics(context.uuid,"allDocuments")
-      let db = client.db(dbName)
+      analytics(context, "allDocuments");
+      let db = client.db(dbName);
       const fieldArr = {
-        _id: '_id',
-        hypothesis_information: 'extracted_information.hypothesis_information',
-        interaction_type: 'extracted_information.interaction_type',
-        negative_information: 'extracted_information.negative_information',
-        pmc_id: 'pmc_id',
-        trigger: 'trigger',
-        evidence: 'evidence',
-        Species: 'extracted_information.context.Species',
+        _id: "_id",
+        hypothesis_information: "extracted_information.hypothesis_information",
+        interaction_type: "extracted_information.interaction_type",
+        negative_information: "extracted_information.negative_information",
+        pmc_id: "pmc_id",
+        trigger: "trigger",
+        evidence: "evidence",
+        Species: "extracted_information.context.Species"
         // entity_text : "extracted_information.participant_a/b.entity_text",
         // entity_type : "extracted_information.participant_a/b.entity_type",
         // identifier : "extracted_information.participant_a/b.identifier"
-      }
-      let andArray = []
-      let orArray = []
-      let regx, tempRes
+      };
+      let andArray = [];
+      let orArray = [];
+      let regx, tempRes;
       for (let arg in args) {
-        regx = new RegExp(args[arg], 'i')
+        regx = new RegExp(args[arg], "i");
         switch (arg) {
-          case 'page':
-            break
-          case 'entity_type':
-            orArray = []
+          case "page":
+            break;
+          case "entity_type":
+            orArray = [];
             orArray.push(
               {
-                'extracted_information.participant_a.entity_type': regx,
+                "extracted_information.participant_a.entity_type": regx
               },
               {
-                'extracted_information.participant_b.entity_type': regx,
+                "extracted_information.participant_b.entity_type": regx
               }
-            )
+            );
             andArray.push({
-              $or: orArray,
-            })
-            break
-          case 'entity_text':
-            orArray = []
+              $or: orArray
+            });
+            break;
+          case "entity_text":
+            orArray = [];
             orArray.push(
               {
-                'extracted_information.participant_a.entity_text': regx,
+                "extracted_information.participant_a.entity_text": regx
               },
               {
-                'extracted_information.participant_b.entity_text': regx,
+                "extracted_information.participant_b.entity_text": regx
               }
-            )
+            );
             andArray.push({
-              $or: orArray,
-            })
-            break
-          case 'identifier':
-            orArray = []
+              $or: orArray
+            });
+            break;
+          case "identifier":
+            orArray = [];
             orArray.push(
               {
-                'extracted_information.participant_a.identifier': regx,
+                "extracted_information.participant_a.identifier": regx
               },
               {
-                'extracted_information.participant_b.identifier': regx,
+                "extracted_information.participant_b.identifier": regx
               }
-            )
+            );
             andArray.push({
-              $or: orArray,
-            })
-            break
+              $or: orArray
+            });
+            break;
           default:
-            let fieldObj = {}
+            let fieldObj = {};
             if (
-              arg == 'negative_information' ||
-              arg == 'hypothesis_information'
+              arg == "negative_information" ||
+              arg == "hypothesis_information"
             )
-              fieldObj[fieldArr[arg]] = args[arg]
-            else if (arg == '_id') {
-              args.page = 0
-              fieldObj['_id'] = new ObjectId(args._id)
-            } else fieldObj[fieldArr[arg]] = regx
-            andArray.push(fieldObj)
-            break
+              fieldObj[fieldArr[arg]] = args[arg];
+            else if (arg == "_id") {
+              args.page = 0;
+              fieldObj["_id"] = new ObjectId(args._id);
+            } else fieldObj[fieldArr[arg]] = regx;
+            andArray.push(fieldObj);
+            break;
         }
       }
-      let filter = {}
-      if (andArray.length > 0) filter['$and'] = andArray
-      let res
+      let filter = {};
+      if (andArray.length > 0) filter["$and"] = andArray;
+      let res;
       res = await db
         .collection(collection)
         .aggregate(
           [
             { $match: filter },
             { $skip: args.page < 1 ? 0 : (args.page - 1) * 250 },
-            { $limit: 250 },
+            { $limit: 250 }
             //   {$lookup: {
             //     'from': 'pubmed',
             //     'let': {
@@ -149,34 +157,34 @@ const resolvers = {
             // }
           ],
           {
-            allowDiskUse: true,
+            allowDiskUse: true
           }
         )
-        .toArray()
-      client.close()
-      return res
+        .toArray();
+      client.close();
+      return res;
     }),
   // It returns PubMed details by PMCID
   getPubMedDetails: (args, context) =>
     context.db().then(async client => {
-      analytics(context.uuid,"getPubMedDetails")
-      let db = client.db(dbName)
-      let query = args.pmcid.toUpperCase()
+      analytics(context, "getPubMedDetails");
+      let db = client.db(dbName);
+      let query = args.pmcid.toUpperCase();
       // let res = await db.collection("pubmed").find({"pmcid" : query}).limit(1)
       let res = await db
-        .collection('pubmed')
-        .findOne({ $or: [{ pmcid: query }, { pmcid: 'PMC' + query }] })
-      client.close()
-      return res
+        .collection("pubmed")
+        .findOne({ $or: [{ pmcid: query }, { pmcid: "PMC" + query }] });
+      client.close();
+      return res;
     }),
   // Returns all the document of the specified identifier
   documentsByIdentifier: (args, context) =>
     context.db().then(client => {
-      analytics(context.uuid,"documentsByIdentifier")
-      let db = client.db(dbName)
-      const id = args.identifier.trim()
-      const regx = id
-      let nameArr = []
+      analytics(context, "documentsByIdentifier");
+      let db = client.db(dbName);
+      const id = args.identifier.trim();
+      const regx = id;
+      let nameArr = [];
       return db
         .collection(collection)
         .aggregate(
@@ -185,108 +193,108 @@ const resolvers = {
               $match: {
                 $or: [
                   {
-                    'extracted_information.participant_a.identifier': regx,
+                    "extracted_information.participant_a.identifier": regx
                   },
                   {
-                    'extracted_information.participant_b.identifier': regx,
-                  },
-                ],
-              },
+                    "extracted_information.participant_b.identifier": regx
+                  }
+                ]
+              }
             },
             {
               $lookup: {
-                from: 'pubmed',
+                from: "pubmed",
                 let: {
-                  pmc: '$pmc_id',
+                  pmc: "$pmc_id"
                 },
                 pipeline: [
                   {
                     $match: {
                       $expr: {
                         $or: [
-                          { $eq: ['$pmcid', { $concat: ['PMC', '$$pmc'] }] },
-                          { $eq: ['$pmcid', '$$pmc'] },
-                        ],
-                      },
-                    },
-                  },
+                          { $eq: ["$pmcid", { $concat: ["PMC", "$$pmc"] }] },
+                          { $eq: ["$pmcid", "$$pmc"] }
+                        ]
+                      }
+                    }
+                  }
                 ],
-                as: 'pubmed',
-              },
+                as: "pubmed"
+              }
             },
             {
               $addFields: {
                 publication_year: {
                   $ifNull: [
                     {
-                      $arrayElemAt: ['$pubmed.year', 0],
+                      $arrayElemAt: ["$pubmed.year", 0]
                     },
-                    '',
-                  ],
+                    ""
+                  ]
                 },
                 journal_title: {
                   $ifNull: [
                     {
-                      $arrayElemAt: ['$pubmed.journal_title', 0],
+                      $arrayElemAt: ["$pubmed.journal_title", 0]
                     },
-                    '',
-                  ],
-                },
-              },
+                    ""
+                  ]
+                }
+              }
             },
             {
               $sort: {
-                publication_year: -1,
-              },
-            },
+                publication_year: -1
+              }
+            }
           ],
           {
-            allowDiskUse: true,
+            allowDiskUse: true
           }
         )
         .toArray()
         .then(arr => {
-          client.close()
+          client.close();
           return {
             documents: arr,
             count: arr.length,
-            searchkey: id,
-          }
-        })
+            searchkey: id
+          };
+        });
     }),
   //	It returns details of all Entities present in database by identifiers.
   allIdentifiers: (args, context) =>
     context.db().then(async client => {
-      analytics(context.uuid,"allIdentifiers")
-      let db = client.db(dbName)
+      analytics(context, "allIdentifiers");
+      let db = client.db(dbName);
       let res = await db
-        .collection('identifier_mapping')
+        .collection("identifier_mapping")
         .find()
-        .toArray()
-      client.close()
-      let limit = args['limit'] > 0 ? args['limit'] : res.length
-      let x, y
+        .toArray();
+      client.close();
+      let limit = args["limit"] > 0 ? args["limit"] : res.length;
+      let x, y;
       return res
         .sort((a, b) => {
-          x = a.iden
-          y = b.iden
-          if (x < y) return -1
-          else if (x > y) return 1
-          else return 0
+          x = a.iden;
+          y = b.iden;
+          if (x < y) return -1;
+          else if (x > y) return 1;
+          else return 0;
         })
-        .slice(0, limit)
+        .slice(0, limit);
     }),
   //	It returns Entity details by identifier
   identifier: (args, context) =>
     context.db().then(async client => {
-      analytics(context.uuid,"identifier")
-      let db = client.db(dbName)
+      analytics(context, "identifier");
+      let db = client.db(dbName);
       let res = await db
-        .collection('identifier_mapping')
-        .findOne({ iden: args.identifier })
-      client.close()
-      return res
-    }),
-}
+        .collection("identifier_mapping")
+        .findOne({ iden: args.identifier });
+      client.close();
+      return res;
+    })
+};
 
-export { resolvers, context }
+export { resolvers, context };
